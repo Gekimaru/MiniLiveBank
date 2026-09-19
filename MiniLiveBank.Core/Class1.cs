6b6b90581368eb19433ceb1454ee66894dbc98b9
Grzegorz <gekimarucoding@gmail.com>
2026-09-19 14:27:27 +0200

    Add project files.

---
+++ MiniLiveBank.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace MiniLiveBank.Core;
+
+public class Class1
+{
+
+}
---
+++ MiniLiveBank.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniLiveBank.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
